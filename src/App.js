6c22a7968b1b7ef7a130eff1dde02e8ee00de9d0
Dana Sharon <whosedreamisthis@gmail.com>
2024-05-24 14:06:33 -0700

    created a react app and added clothing categories with just h1 tags for now

---
+++ src/App.js
@@ -0,0 +1,38 @@
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container"></div>
+      {/* <img /> */}
+      <div className="category-body-container">
+        <h2>Hats</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container"></div>
+      {/* <img /> */}
+      <div className="category-body-container">
+        <h2>Jackets</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container"></div>
+      {/* <img /> */}
+      <div className="category-body-container">
+        <h2>Sneakers</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container"></div>
+      {/* <img /> */}
+      <div className="category-body-container">
+        <h2>Womens</h2>
+        <p>Shop Now</p>
+      </div>
+      <div className="category-container"></div>
+      {/* <img /> */}
+      <div className="category-body-container">
+        <h2>Mens</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
