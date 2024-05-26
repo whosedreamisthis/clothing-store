fb067c9ad8bda9f8b62a710c40921f7dbe479486
Dana Sharon <whosedreamisthis@gmail.com>
2024-05-25 18:59:04 -0700

    authentication tweaks

--- src/components/navigation/nav-bar.component.jsx
+++ src/components/navigation/nav-bar.component.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import "./nav-bar.styles.scss";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 const NavBar = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <div className="nav-bar">
       <Link className="logo-container" to="/home">
@@ -16,9 +21,15 @@ const NavBar = () => {
         <Link className="nav-link" to="/shop">
           Contact
         </Link>
-        <Link className="nav-link" to="/auth">
-          Sign In
-        </Link>
+        {currentUser ? (
+          <span onClick={signOutUser} className="nav-link">
+            SIGN OUT
+          </span>
+        ) : (
+          <Link className="nav-link" to="/auth">
+            Sign In
+          </Link>
+        )}
       </div>
 
       {/* <Outlet /> */}
